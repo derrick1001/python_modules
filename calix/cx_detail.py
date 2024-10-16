c3cf0d9de88119498807dd480ab8f3559412f850
derrick <twohackerauth@proton.me>
2024-10-16 13:26:01 -0500

    Testing out some docstrings

--- calix/cx_detail.py
+++ calix/cx_detail.py
@@ -2,13 +2,20 @@ from requests import get
 
 
 # NOTE: Returns dict of customer detail
-def cx_detail(e9, ont_id):
+def cx(e9, ont_id):
     """
-    This function requires the ONT id
+    *************************
+    This function requires the E9 hostname and the ONT id
+
+    e9: str
+    ont_id: str
+
+    Ex: cx('hostname', '1111')
+    *************************
     """
-    cx = get(
+    cx_detail = get(
         f"https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{e9}/port/{ont_id}%2Fx1",
         auth=("admin", "Thesearethetimes!"),
         verify=False,
     )
-    return cx
+    return cx_detail.json()
--- calix/ont_detail.py
+++ calix/ont_detail.py
@@ -2,10 +2,20 @@ from requests import get
 
 
 # NOTE: Returns dict of ont detail
-def ont_detail(e9, ont_id):
-    ont = get(
+def ont(e9, ont_id):
+    """
+    *************************
+    This function requires the E9 hostname and the ONT id
+
+    e9: str
+    ont_id: str
+
+    Ex: cx('hostname', '1111')
+    *************************
+    """
+    ont_detail = get(
         f"https://10.20.7.10:18443/rest/v1/performance/device/{e9}/ont/{ont_id}/status",
         auth=("admin", "Thesearethetimes!"),
         verify=False,
     )
-    return ont
+    return ont_detail.json()
