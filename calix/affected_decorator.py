1bc7c07e46d1167ff552593f417890b887a14800
Derrick Ishman <derrick.ishman@gmail.com>
2024-10-18 14:06:47 -0500

    affected_decorator module should be completed

--- calix/affected_decorator.py
+++ calix/affected_decorator.py
@@ -1,43 +1,37 @@
 from calix.cx_detail import cx
-from calix.affected import affected
 
 # NOTE:
-#   This decaorator takes in the instance-id from
-#   the alarm table, along with the ont-id and will
-#   put together a list of customers that are without
-#   service. It can be used with any function that
-#   processes alarms and needs to get the affected
-#   subscribers from the hyperlink. It also uses
-#   the ont-id to get the phone number if applicable.
-
-# WARN:
-#   The API call 'affected', does not align with the active alarm table
-#   and shouldnt be used. Rewrite using the 'cx_detail' module and make the
-#   list yourself
+#   This decorator takes in an iterable and calls
+#   cx function from calix.cx_detail to put together
+#   our own affected subscriber output
 
 
 def affected_decorator(func):
     def inner(*args, **kwargs):
-        account = (affected(args[0], id).text.split("\r\n")[1:-1] for id in args[1])
-        try:
-            for sub, id in zip(account, args[2]):
-                r1 = cx(args[0], id)
-        except IndexError:
+        ont_ids = func()
+        account = (cx(kwargs.get('e9'), id) for id in ont_ids)
+        for sub in account:
             try:
-                phone = r1.get("locations")[0].get("contacts")[0].get("phone")
-                if phone is None:
-                    phone = "No phone"
+                phone = sub.get("locations")[0].get("contacts")[0].get("phone")
             except Exception:
                 phone = "No phone"
-            splt_i = sub[0].split(",")
-            acct, name, loc, em = (
-                splt_i[0],
-                splt_i[1],
-                " ".join(splt_i[2:5]),
-                splt_i[-1],
-            )
-            if em == "":
+            else:
+                if phone is None:
+                    phone = "No phone"
+            try:
+                em = sub.get("locations")[0].get("contacts")[0].get("email")
+            except Exception:
                 em = "No email"
+            else:
+                if em is None:
+                    em = "No email"
+            try:
+                loc = sub.get("locations")[0].get("address")[0].get(
+                    "streetLine1") + ', ' + sub.get("locations")[0].get("address")[0].get("city")
+            except Exception:
+                loc = 'No address'
+            name = sub.get("name")
+            acct = sub.get("customId")
             print(f"{acct}\n{name}\n{phone}\n{em}\n{loc}\n")
         return
 
