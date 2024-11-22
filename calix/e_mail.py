2a9a6912689f495fb9fb2531901c0d8330bdfaef
Derrick Ishman <derrick.ishman@gmail.com>
2024-11-22 16:27:36 -0600

    Added cc group for email

--- calix/e_mail.py
+++ calix/e_mail.py
@@ -8,7 +8,7 @@ def email(subj: str, cont: str) -> None:
     msg["Subject"] = subj
     msg["From"] = "nms@mycvecfiber.com"
     msg["To"] = "dishman@cvecfiber.com"
-    # msg['Cc'] = ['kmarshala@cvecfiber.com', 'jjackson@cvecfiber.com']
+    msg["Cc"] = ["kmarshala@cvecfiber.com", "jjackson@cvecfiber.com"]
     s = smtplib.SMTP("10.20.7.31")
     s.send_message(msg)
     s.quit()
