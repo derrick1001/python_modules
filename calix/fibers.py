633a9a83a7dddad154331062d60f4b8c79d7cf8f
Derrick Ishman <derrick.ishman@gmail.com>
2025-01-17 09:12:47 -0600

    fix: get_fibers.py function is complete

--- calix/fibers.py
+++ calix/fibers.py
@@ -1,14 +1,23 @@
 from calix.connection import calix_e9
 
+# Make a list comp of port descriptions
+# Order is important, if any IndexError, return None
+# Convert to generator comp and return
+
 
 def get_fibers(pon_port: list):
     cnct = calix_e9()
     cnct.send_command_timing("configure")
-    fibers = (
-        cnct.send_command_timing(
-            f"show full-configuration interface pon {port} | inc description"
-        ).split()[1]
-        for port in pon_port
-    )
+    try:
+        fibers = [
+            cnct.send_command_timing(
+                f"show full-configuration interface pon {port} | inc description"
+            ).split()[1]
+            for port in pon_port
+        ]
+    except IndexError:
+        cnct.disconnect()
+        return None
     cnct.send_command_timing("top")
+    fibers = (fiber for fiber in fibers)
     return fibers
