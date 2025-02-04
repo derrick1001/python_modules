14858510c9a4556a6c439bdf696baca9d42008cc
Derrick Ishman <twohackerauth@proton.me>
2025-02-04 11:07:59 -0600

    fix: fixed lop alarm output so it grabs all re matches

--- calix/proc_alrms.py
+++ calix/proc_alrms.py
@@ -17,16 +17,15 @@ def proc_alarms(func):
                 m.group().lstrip("'").rstrip("'") for m in match_pon if m is not None
             )
             cnct = calix_e9()
-            sub_on_port = [
+            sub_on_port = (
                 cnct.send_command_timing(
-                    f"show interface pon {port} subscriber-info | display curly-braces | include ont"
+                    f"show interface pon {port} subscriber-info | display curly-braces | inc ont-id",
+                    strip_prompt=True,
                 )
                 for port in pon_port
-            ]
-            match_ont = [re.search("[0-9]{1,5}", id) for id in sub_on_port]
-            ont_id = [
-                m.group().lstrip("'").rstrip("'") for m in match_ont if m is not None
-            ]
+            )
+            pattern = re.compile(r"[0-9]{3,5}")
+            ont_id = (pattern.findall(id) for id in sub_on_port)
             return ont_id
         else:
             match_ont = [
