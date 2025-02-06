42e9346a6f6834c30b5de9ac1b2abb12eca00e16
Derrick Ishman <derrick.ishman@gmail.com>
2025-02-06 14:51:06 -0600

    fix: fixed bug where 'x1' interface is 'G1', resulting in None return value from the function.

--- calix/cx_detail.py
+++ calix/cx_detail.py
@@ -16,4 +16,10 @@ def cx(e9, ont_id):
         auth=("admin", "Thesearethetimes!"),
         verify=False,
     )
+    if cx_detail.status_code == 404:
+        cx_detail = get(
+            f"https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{e9}/port/{ont_id}%2FG1",
+            auth=("admin", "Thesearethetimes!"),
+            verify=False,
+        )
     return cx_detail.json()
