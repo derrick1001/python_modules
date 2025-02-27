33366d43b34fbaa4a3736c80d3530516231c3f3b
Derrick Ishman <derrick.ishman@gmail.com>
2025-02-27 10:54:51 -0600

    added check for lop in subj and set cc group accordingly

--- calix/e_mail.py
+++ calix/e_mail.py
@@ -2,13 +2,14 @@ import smtplib
 from email.message import EmailMessage
 
 
-def email(subj: str, cont: str) -> None:
+def email(subj: str, cont) -> None:
     msg = EmailMessage()
     msg.set_content(cont)
     msg["Subject"] = subj
     msg["From"] = "nms@mycvecfiber.com"
     msg["To"] = "dishman@cvecfiber.com"
-    msg["Cc"] = ["kmarshala@cvecfiber.com", "jjackson@cvecfiber.com"]
+    if "Loss of PON" not in subj:
+        msg["Cc"] = ["kmarshala@cvecfiber.com", "jjackson@cvecfiber.com"]
     s = smtplib.SMTP("10.20.7.31")
     s.send_message(msg)
     s.quit()
