c93ed2c9f2c0505da72af32d0eba7f36803cb2b0
Derrick Ishman <derrick.ishman@gmail.com>
2025-04-08 16:17:09 -0500

    feat: python script for DELETE method to delete given ONT

---
+++ calix/del_ont.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3.13
+
+from requests import delete
+
+
+def del_ont(id: str, e9: str = "CVEC-E9-1"):
+    """
+    API DELETE method that deletes the given ONT id
+
+    Parameters: id: str, e9: str
+
+    Returns nothing
+    """
+
+    del_ont = delete(
+        f"https://10.20.7.10:18443/rest/v1/config/device/{e9}/ont?ont-id={id}&force-delete=true",
+        auth=("admin", "Thesearethetimes!"),
+        verify=False,
+    )
