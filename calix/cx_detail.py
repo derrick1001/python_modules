724eee1bf376f9b889426da2e52d5002e8d0e4aa
Derrick Ishman <twohackerauth@proton.me>
2026-01-21 10:28:18 -0600

    added a few annotations

--- calix/cx_detail.py
+++ calix/cx_detail.py
@@ -1,7 +1,7 @@
 from requests import get
 
 
-def cx(e9, ont_id):
+def cx(e9: str, ont_id: str) -> dict:
     """
     This function gets subscriber info using the ONT id
 
@@ -9,10 +9,11 @@ def cx(e9, ont_id):
         e9: str
         ont_id: str
 
-    Returns dictionary of all subscriber data
+    Returns dictionary of all subscriber data if successfull
     """
     cx_detail = get(
-        f"https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{e9}/port/{ont_id}%2Fx1",
+        f"https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{
+            e9}/port/{ont_id}%2Fx1",
         auth=("admin", "Thesearethetimes!"),
         verify=False,
     )
--- calix/ont_detail.py
+++ calix/ont_detail.py
@@ -1,7 +1,7 @@
 from requests import get
 
 
-def ont(e9, ont_id):
+def ont(e9: str, ont_id: str):
     """
     This function gets ONT detail using the ONT id
 
@@ -12,7 +12,8 @@ def ont(e9, ont_id):
     Returns dictionary of all ONT data
     """
     ont_detail = get(
-        f"https://10.20.7.10:18443/rest/v1/performance/device/{e9}/ont/{ont_id}/status",
+        f"https://10.20.7.10:18443/rest/v1/performance/device/{
+            e9}/ont/{ont_id}/status",
         auth=("admin", "Thesearethetimes!"),
         verify=False,
     )
